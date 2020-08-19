7a8ca93db3f44fc7721a7decbdf4b396bdcf59fc
Oakes <kennyolapower@gmail.com>
2020-08-19 19:58:57 +0200

    BookList

--- appes6.js
+++ appes6.js
@@ -56,6 +56,50 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+   static getBooks() {
+   let books;
+   if(localStorage.getItem('books') === null) {
+     books = [];
+   } else {
+     books = JSON.parse(localStorage.getItem('books'));
+   }
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+      const ui = new UI;
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+   static addBook() {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+      if(book.isbn === isbn){
+       books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get form values
@@ -79,6 +123,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Add book to list
   ui.addBookToList(book);
 
+  // Add to LS
+  Store.addBook(book);
+
   // Show success
   ui.showAlert('Book Added', 'success');
 
@@ -97,6 +144,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
     // Delete book
     ui.deleteBook(e.target);
+
+    // Remove from Localstorage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // Show message
     ui.showAlert('Book removed!', 'success');
